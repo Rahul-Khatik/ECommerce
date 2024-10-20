b0efbdeae0f172bbd25e0635c698239ace3dfb97
Fluuter_Tutorial <rahulkhatik9574@gmail.com>
2024-10-20 16:03:25 +0530

    frontend completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-import { Container } from "react-bootstrap";
-// import Header from "./components/Header";
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Container } from "@mui/material";
 
 function App() {
   return (
@@ -9,10 +12,14 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1> Welcome to ProShop </h1>
+
+          <Box sx={{ padding: "10px" }}>
+            <Outlet />
+          </Box>
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,20 +1,18 @@
 import { Container, Row, Col } from "react-bootstrap";
 
-
-const Footer = ()=>{
+const Footer = () => {
   const currentYear = new Date().getFullYear();
-  console.log(currentYear);
+
   return (
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>
-            <p> ProShop &copy; {currentYear}</p>
+          <Col className="text-center py-3">
+            <p>Embellish &copy; {currentYear}</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
-
+  );
+};
 export default Footer;
